57f2a2c7318e62070516bf2b15a2d4912d1a0d8c
eze <ezequielgaray37@gmail.com>
2024-03-17 20:24:06 -0300

    nav en proceso

--- src/App.js
+++ src/App.js
@@ -5,15 +5,21 @@ import Aspiradoras from './pages/Aspiradoras/aspiradoras';
 import Cocinas from './pages/cocinas/cocinas'
 import Soporte from './pages/Suport/soporte'
 import Home from './pages/Home/home'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <Navbar/>
-      <Home/>
-      <Aspiradoras/>
-      <Cocinas/>
-      <Soporte/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/Home" element={<Home/>} />
+        <Route path="/Aspiradoras" element={<Aspiradoras/>} />
+        <Route path="/Cocinas" element={<Cocinas/>} />
+        <Route path="/Soporte" element={<Soporte/>} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,41 +2,81 @@ import React, { useState } from 'react';
 import Style from './Navbar.module.css';
 import { FiAlignJustify } from "react-icons/fi";
 import logo from './assests/logo.png';
-import { Link } from "react-scroll";
+import { Link } from 'react-router-dom';
+import { TbSearch } from "react-icons/tb";
+import { FaRegCommentDots } from "react-icons/fa";
 
 const Navbar = () => {
     const [navBarOpen, setNavBarOpen] = useState(false);
+    const [activeLink, setActiveLink] = useState(null);
 
     const links = [
-        { id: 1, link: "Aspiradoras" },
-        { id: 2, link: "Cocinas" },
-        { id: 3, link: "Soporte" },
-        { id: 4, link: "Home" }
+        { id: 1, link: "Aspiradoras", to: "/Aspiradoras", options: ["Opción 1", "Opción 2", "Opción 3"] },
+        { id: 2, link: "Cocinas", to: "/Cocinas", options: ["Opción 4", "Opción 5", "Opción 6"] },
+        { id: 3, link: "Soporte", to: "/Soporte", options: [] },
+        { id: 4, link: "Home", to: "/Home", options: [] }
     ];
 
+    const handleLinkClick = (linkId) => {
+        if (activeLink === linkId) {
+            setActiveLink(null); 
+        } else {
+            setActiveLink(linkId); 
+        }
+    };
+
     return (
-        <div >
+        <div>
             <div className={Style.navbar}>
-            <img className={Style.logo} src={logo} alt='logo'/>
-            <button className={Style.categories} onClick={() => setNavBarOpen(!navBarOpen)}>
-                <FiAlignJustify size={27} />
-                <h1>Categorias</h1>
-            </button>
+                <div className={Style.navbar1}>
+                    <img className={Style.logo} src={logo} alt='logo'/>
+                </div>
+                <div className={Style.navbar2}>
+                    <button className={Style.categories} onClick={() => setNavBarOpen(!navBarOpen)}>
+                        <FiAlignJustify size={30} />
+                        <p className={Style.categoriesTex} >Categorias</p>
+                    </button>
+                </div>
+                <div className={Style.navbar3}>
+                    <form className={Style.search_form}>
+                        <input className={Style.search_box} type='text' placeholder='     ¿Qué buscas?'/>
+                        <button className={Style.search_button}><TbSearch size={20}/></button>
+                    </form>
+                </div>
+                <div className={Style.navbar4}>
+                    <Link to="/contactar" className={Style.contact_button}>
+                        <FaRegCommentDots size={25} />
+                        <h4>Contactar</h4>
+                    </Link>
+                </div>
             </div>
 
-            <div>
-            {navBarOpen && (
-                <ul>
-                    {links.map((link) => (
-                        <li key={link.id}>
-                            <Link to={link.link}>{link.link}</Link>
-                        </li>
-                    ))}
+            <div className={navBarOpen ? `${Style.navbarOpen}` : ''}>
+            <img className={`${Style.logo2}`} src={logo} alt='logo'/>
+            <button className={`${Style.closeButton}`} onClick={() => setNavBarOpen(false)}>×</button>
+            <ul>
+                {links.map((link) => (
+                    <li key={link.id}>
+                        <Link
+                            to={link.to}
+                            onClick={() => handleLinkClick(link.id)} 
+                            className={activeLink === link.id ? Style.activeLink : null} 
+                        >
+                            {link.link}
+                        </Link>
+                        {activeLink === link.id && (
+                            <ul>
+                                {link.options.map((option, index) => (
+                                    <li key={index}>{option}</li>
+                                ))}
+                            </ul>
+                        )}
+                    </li>
+                ))}
                 </ul>
-            )}
             </div>
+
         </div>
-        
     );
 };
 
